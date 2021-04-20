307011ff7fd8d5e0a2aad27d3fe006d0f6793edb
Ram04042 <yadavramakrishnan@gmail.com>
2021-04-20 09:43:44 +0530

    Added Market Price

--- src/app/crud-api.service.ts
+++ src/app/crud-api.service.ts
@@ -94,6 +94,11 @@ export class CRUDApiService {
     return this.httpClient.get<any>(this.apiServer + '/Admin?Claim_no='+Claim_no)
   }
 
+  getMarketPriceApi(Model_Name): Observable<any> {
+    const opts = { params: new HttpParams({fromString: "Model_Name="+ Model_Name}) };
+    return this.httpClient.get<any>(this.apiServer + '/MarketPrice/?model_name='+Model_Name)
+  }
+
   updateclaims(Claim_no,claim_info): Observable<any>{
     return this.httpClient.put<any>(this.apiServer + '/Admin?Claim_no='+Claim_no, JSON.stringify(claim_info), this.httpOptions)
   }
